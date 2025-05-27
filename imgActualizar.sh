b6b699aafc49429a61b4739042ab24446f8c7cc0
Cris Álvarez <167922974+crisalvrz@users.noreply.github.com>
2025-05-27 12:44:56 +0200

    Update imgActualizar.sh

--- imgActualizar.sh
+++ imgActualizar.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-
-# Obtener todas las imágenes
 imagenes=$(docker images -q)
-
-# Actualizar las imágenes
 for imagen in $imagenes; do
     docker pull "$imagen"
 done
 
-# Reiniciar contenedores con nuevas imágenes
 contenedores=$(docker ps -q)
 for contenedor in $contenedores; do
     docker stop "$contenedor"
     docker start "$contenedor"
 done
 
-echo "Todas las imágenes y contenedores han sido actualizados."
+echo "Ya han sido actualizados."
